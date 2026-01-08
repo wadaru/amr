5e6defad23e6e0426121bb15540cbf4520893ccb
Wataru Uemura <wataru@kdel.org>
2026-01-09 01:46:19 +0900

    update the cmd_vel for Robotino3

--- gazebo/docker/teleop_keyboard_2dw1c.sh
+++ gazebo/docker/teleop_keyboard_2dw1c.sh
@@ -2,6 +2,6 @@
 source /opt/ros/jazzy/setup.bash
 ros2 run ros_gz_bridge parameter_bridge \
   /cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist \
-  /model/simple_robot/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry \
-  --ros-args -r /model/simple_robot/odometry:=/odom & 
+  /model/robot_2dw1c/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry \
+  --ros-args -r /model/robot_2dw1c/odometry:=/odom & 
 ros2 run teleop_twist_keyboard teleop_twist_keyboard
