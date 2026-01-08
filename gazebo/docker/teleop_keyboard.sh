c19de5feb7b0456629b4699c0d168aad4c27aa31
Wataru Uemura <wataru@kdel.org>
2026-01-08 23:53:06 +0900

    add the topic of /odom

--- gazebo/docker/teleop_keyboard.sh
+++ gazebo/docker/teleop_keyboard.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 source /opt/ros/jazzy/setup.bash
-ros2 run ros_gz_bridge parameter_bridge /cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist &
+ros2 run ros_gz_bridge parameter_bridge \
+  /cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist \
+  /model/simple_robot/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry \
+  --ros-args -r /model/simple_robot/odometry:=/odom & 
 ros2 run teleop_twist_keyboard teleop_twist_keyboard
