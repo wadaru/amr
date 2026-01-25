8d648a573669d52d03b5f0bf43301624b4bbc085
Wataru Uemura <wataru@kdel.org>
2026-01-25 22:22:37 +0100

    add ":"

--- gazebo/docker/scripts/teleop_keyboard_2dw1c.sh
+++ gazebo/docker/scripts/teleop_keyboard_2dw1c.sh
@@ -5,4 +5,4 @@ ros2 run ros_gz_bridge parameter_bridge \
   /$robotName/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist \
   /model/$robotName/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry \
   --ros-args -r /model/$robotName/odometry:=/$robotName/odom & 
-ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r cmd_vel=/$robotName/cmd_vel
+ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r cmd_vel:=/$robotName/cmd_vel
