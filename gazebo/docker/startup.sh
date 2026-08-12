4edccd36457c387cc8673b7cc07b8795ece27437
Wataru UEMURA <kotora@Mac-mini.local>
2026-08-12 16:26:20 +0900

    update for VS Code

---
+++ gazebo/docker/startup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# for ssh server
+sudo /etc/init.d/ssh start
+
+# for ROS2 colcon_ws
+if [ ! -f /home/ubuntu/colcon_ws/install/setup.bash ]; then
+    mkdir -p /home/ubuntu/colcon_ws/src
+
+    ln -sf \
+      /home/ubuntu/git/amr/gazebo/amr_robot_launcher \
+      /home/ubuntu/colcon_ws/src/amr_robot_launcher
+
+    chown -R ubuntu:ubuntu /home/ubuntu/colcon_ws
+
+    sudo -u ubuntu bash -c '
+        source /opt/ros/jazzy/setup.bash
+        cd ~/colcon_ws
+        colcon build
+    '
+fi
+
+exec /entrypoint.sh
